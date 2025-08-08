ad5b89f7d9e7d81ca7f4628a553a7e583171e748
mizepark <70455627+mizepark@users.noreply.github.com>
2025-08-09 08:40:52 +0900

    Update build_ios.sh

--- master/packages/build_ios.sh
+++ master/packages/build_ios.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 set -e
-echo "=> Placeholder for iOS build script"
+echo "=> Building iOS framework"
+
+OUTPUT_DIR=./packages/build/Release-universal
+ARCHIVE_PATH=${OUTPUT_DIR}/KataGo.xcarchive
+
+rm -rf ${OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
+
+# 루트에 .xcodeproj 또는 .xcworkspace 파일 이름을 확인해서 아래를 고칠 것
+xcodebuild \
+  -workspace KataGo.xcworkspace \
+  -scheme KataGo \
+  -configuration Release \
+  -archivePath ${ARCHIVE_PATH} \
+  archive
+
+xcodebuild \
+  -create-xcframework \
+  -framework ${ARCHIVE_PATH}/Products/Library/Frameworks/KataGo.framework \
+  -output ${OUTPUT_DIR}/KataGo-iOS-OS64COMBINED.zip
+
+echo "=> Done, zip is at ${OUTPUT_DIR}/KataGo-iOS-OS64COMBINED.zip"
